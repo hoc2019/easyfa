dfe104c16c7ce3240b8a34301b58f77bf9805836
neal <neal.wang@pdabc.com>
2019-04-22 11:28:05 +0800

    demo页 路径

---
+++ config/webpack.doc.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+module.exports = {
+    mode: 'production',
+    entry: './example/app.tsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../docs'),
+        publicPath: '/easyfa'
+    },
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
+            },
+            {
+                test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                loader: require.resolve('url-loader'),
+                options: {
+                    limit: 10000,
+                    name: 'images/[name].[hash:8].[ext]'
+                }
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
+        ]
+    }
+};
