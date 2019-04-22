7022a95762f7ddda383576d5159be20fdd016387
neal <neal.wang@pdabc.com>
2019-04-22 11:29:36 +0800

    图片路径

--- config/webpack.doc.config.js
+++ config/webpack.doc.config.js
@@ -5,7 +5,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, '../docs'),
-        publicPath: '/easyfa'
+        publicPath: '/easyfa/'
     },
 
     resolve: {
